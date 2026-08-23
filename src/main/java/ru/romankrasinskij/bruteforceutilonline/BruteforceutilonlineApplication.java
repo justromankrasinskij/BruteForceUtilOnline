733da78c58ff9d3d5aff48172b2def6b35310219
Roman Krasinskij <justromankrasinskij@gmail.com>
2026-08-23 15:35:34 +0300

    Add Spring project files

---
+++ src/main/java/ru/romankrasinskij/bruteforceutilonline/BruteforceutilonlineApplication.java
@@ -0,0 +1,13 @@
+package ru.romankrasinskij.bruteforceutilonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruteforceutilonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruteforceutilonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/romankrasinskij/bruteforceutilonline/BruteforceutilonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romankrasinskij.bruteforceutilonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruteforceutilonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
